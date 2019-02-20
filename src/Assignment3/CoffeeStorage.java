dd34eaa5571f7bfc9c0619acf99f0592e8da9c5b
Kasper <kasper.m.witt@outlook.com>
2019-02-20 22:34:15 +0100

    0.0.6 - Kasper added comments and shit.

--- src/Assignment3/CoffeeStorage.java
+++ src/Assignment3/CoffeeStorage.java
@@ -3,22 +3,25 @@ package Assignment3;
 import java.util.Vector;
 
 public class CoffeeStorage {
-    private Vector<Coffee> coffeeStorage = new Vector(20);
+    private Vector<Coffee> coffeeStorage = new Vector();
 
     CoffeeStorage(){
     }
 
+    // Fill the coffeeStorage with int random cups of coffee.
     public void fillCoffeeStorage(int cupsToAdd){
         for(int i = cupsToAdd; i > 0; i--){
             addOneRandomCoffee();
         }
     }
 
+    // Get one random cup of coffee from the storage.
     public Coffee getOneCup(){
         int random = (int)(Math.random() * coffeeStorage.size());
         return coffeeStorage.get(random);
     }
 
+    // Add one random cup of coffee to the storage.
     private void addOneRandomCoffee(){
         int random = (int)(Math.random() * 3) + 1;
         if(random == 1) {
--- src/Assignment3/Main.java
+++ src/Assignment3/Main.java
@@ -3,6 +3,7 @@ package Assignment3;
 public class Main {
 
     public static void main(String[] args) {
+        // Example use of the coffee-machine.
         CoffeeStorage Machine = new CoffeeStorage();
         Machine.fillCoffeeStorage(20);
         Machine.getOneCup().getEnergyValue();
