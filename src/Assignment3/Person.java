0d6e01021413832e1c29d3d90eb9357999edb565
Carl-Simon <46845880+Carl-Simon@users.noreply.github.com>
2019-02-21 13:42:23 +0100

    Merge branch 'master' of /Users/simon/IdeaProjects/OOP2018_a18kaswi_b17carbr_assignment3 with conflicts.

--- src/Assignment3/Person.java
+++ src/Assignment3/Person.java
@@ -1,68 +1,57 @@
 package Assignment3;
 
-import java.io.IOException;
-import java.util.concurrent.*;
-import java.util.concurrent.locks.Lock;
-import java.util.concurrent.locks.ReentrantLock;
+import java.sql.Time;
+import java.util.Timer;
+import java.util.TimerTask;
 
-public class Person implements Runnable{
-    private String name;
-    private int energy;
+public class Person implements Runnable {
+    String name;
+    int energy;
+    Timer timer = new Timer();
+
     CoffeeStorage coffeeMachine = new CoffeeStorage();
-    private static Lock lock = new ReentrantLock();
 
-    public Person(String name){
+    public Person(String name) {
+        coffeeMachine.fillCoffeeStorage(20);
         this.name = name;
-        this.energy = (((int) (Math.random() *  30) + 1));
-    }
-
-    private void decrementEnergyLevel(){
-        if (energy > 0){
-            try{
-                energy = energy - 10;
-                Thread.sleep(1000);
-            } catch(InterruptedException ex){
-                System.out.println(ex);
-            }
-        }
-        else if(energy <= 0){
-            System.out.println("Tired - going home");
-        }
+        this.energy = (((int) (Math.random() * 90) + 30));
     }
 
     @Override
     public void run() {
         if (energy > 0 && energy < 100) {
             System.out.println(name + " has " + energy + " energy and goes to coffee room");
-            decrementEnergyLevel();
         }
-        do {useCoffeeMachine();
-        }
-        while(energy > 0 && energy < 100);
-        if (energy <= 0){
-            System.out.println("Tired going home");
+        while (energy > 0 && energy < 100) {
+            useCoffeeMachine();
+            timer.scheduleAtFixedRate(energyDrained, 1000, 1000);
+
         }
     }
 
-    private void useCoffeeMachine(){
-    lock.lock();
-        while(energy > 0 && energy < 100)
-            setEnergy(coffeeMachine.getOneCup());
+    public void EnergyDrainedTimer() {
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(call, 1000, 1000);
+    synchronized TimerTask call = new TimerTask() {
+        @Override
+        public void run() {
+            energy++;
 
-    lock.unlock();
+        }
+    };
+}
+
+
+    public synchronized void useCoffeeMachine(){
+        setEnergy(coffeeMachine.getOneCup());
     }
 
-    private  void setEnergy(Coffee newCup){
+    public void setEnergy(Coffee newCup){
         energy += newCup.getEnergyValue();
         if(energy > 0 && energy < 100){
             System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy));
         } else if(energy >= 100){
             System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy) + " and goes to office");
-        } else System.out.println("Goes home");
-        try{
-            Thread.sleep(1000);
-        } catch(InterruptedException ex){
-            System.out.println(ex);
         }
     }
 }
