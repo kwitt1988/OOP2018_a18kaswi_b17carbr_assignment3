f0c57baba26fc792715363ff51de534e521a82f9
Carl-Simon <46845880+Carl-Simon@users.noreply.github.com>
2019-02-21 15:32:28 +0100

    Merge branch 'master' of /Users/simon/IdeaProjects/OOP2018_a18kaswi_b17carbr_assignment3 with conflicts.

--- src/Assignment3/Person.java
+++ src/Assignment3/Person.java
@@ -3,9 +3,10 @@ package Assignment3;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class Person implements Runnable, MachineInterface{
+public class Person implements Runnable{
     private String name;
     private int energy;
+    CoffeeStorage coffeeMachine = new CoffeeStorage();
     private static Lock lock = new ReentrantLock();
 
     public Person(String name){
@@ -13,9 +14,27 @@ public class Person implements Runnable, MachineInterface{
         this.energy = (((int) (Math.random() *  60) + 30));
     }
 
+    private void decrementEnergyLevel(){
+        try{
+            this.energy -=10;
+            Thread.sleep(1000);
+        } catch(InterruptedException ex){
+            System.out.println(ex);
+        }
+    }
+
+    private void officeLoop(){
+        while(energy > 30){
+            decrementEnergyLevel();
+            System.out.println(name + " Low on coffee, going back to machine");
+        }
+    }
+
+
     @Override
     public void run() {
         while(energy > 0){
+            System.out.println(name + energy);
             if(lock.tryLock()){
                 useCoffeeMachine();
                 lock.unlock();}
@@ -24,39 +43,29 @@ public class Person implements Runnable, MachineInterface{
             }
             if(energy > 100) officeLoop();
         }
-        System.out.println(name + " is out of energy. Going home.");
+        System.out.println(name + "Tired going home");
     }
 
     private void useCoffeeMachine(){
+        lock.lock();
         while(energy > 0 && energy < 100)
             setEnergy(coffeeMachine.getOneCup());
+        lock.unlock();
     }
 
-    private void decrementEnergyLevel(){
-        this.energy -=10;
-        threadSleep();
-    }
-
-    private void officeLoop(){
-        while(energy > 30){
-            decrementEnergyLevel();
-        }
-        System.out.println(name + " is low on coffee, going back to the coffee-room");
-    }
-
-    private void threadSleep(){
+    private  void setEnergy(Coffee newCup){
+        energy += newCup.getEnergyValue();
+        if(energy > 0 && energy < 100){
+            System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy));
+        } else if(energy >= 100){
+            System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy) + " and goes to office");
+        } else System.out.println("Goes home");
         try{
             Thread.sleep(1000);
         } catch(InterruptedException ex){
             System.out.println(ex);
         }
     }
-
-    private  void setEnergy(Coffee newCup){
-        energy += newCup.getEnergyValue();
-        if(energy > 100){
-            System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy) + " and goes to office");
-        } else System.out.println(name + " consumes a " + newCup.getCoffeeType() + " with " + newCup.getEnergyValue() + " and now has " + (energy));
-        threadSleep();
-    }
 }
+
+
