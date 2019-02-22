7f986f973ae2b5cb172d0cd978f12239380bb6d4
Carl-Simon <46845880+Carl-Simon@users.noreply.github.com>
2019-02-22 02:15:58 +0100

    //SIMON  HAR LAGT TILL 2 I PERSON ATT TESTA MED

--- src/Assignment3/Person.java
+++ src/Assignment3/Person.java
@@ -1,5 +1,6 @@
 package Assignment3;
 
+import java.util.TimerTask;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
@@ -13,11 +14,13 @@ public class Person implements Runnable{
     public Person(String name){
         this.name = name;
         this.energy = (((int) (Math.random() *  60) + 30));
+        //decrementEnergyLevel();  // Check further down
     }
 
     @Override
     public void run() {
         while(true){
+            //theGreateFilter(); // Check further down
             if(energy > 100){
                 officeRoom();
             } else if(energy > 0){
@@ -35,6 +38,53 @@ public class Person implements Runnable{
         System.out.println(name + " is low on energy so goes to get coffee.");
     }
 
+    //private void theGreateFilter(){                    <------- // Filter for threads, not proper solution but better than nothing
+    //        if(energy <= 30 && energy > 0){            <------- // if we cannot find a way to notify threads with low values when
+    //            try{                                   <------- // loop is ready to be used. Or throw away if you wish.
+    //                Thread.sleep(1000);                <------- //
+    //                                                   <------- //
+    //            } catch(InterruptedException ex){               // Uncomment theGreateFilter and decrementEnergyLevel and play with
+    //                System.out.println(ex);                     // the values of each of them to get a satisfying amount of flow and
+                                                                  // and filtering.
+    //            }
+
+    //       }
+
+    //        else if (energy <= 60 && energy > 30){
+    //            try{
+    //                Thread.sleep(2000);
+    //
+    //            } catch(InterruptedException ex){
+    //                System.out.println(ex);
+    //            }
+
+    //        }
+
+    //        else if(energy <= 100 && energy > 60){
+    //            try{
+    //                Thread.sleep(3000);
+    //
+    //            } catch(InterruptedException ex){
+    //                System.out.println(ex);
+    //            }
+
+    //        }
+
+    //        }
+
+      // USE THIS TO SPEED UP FOR TESTING 
+    //public void decrementEnergyLevel() {
+    //    if (energy > 0) {
+    //        new java.util.Timer().schedule(new TimerTask() {
+    //            @Override
+    //            public void run() {
+    //                energy -= 10;
+    //            }
+    //        }, 1000, 1000);
+    //    }
+    //}
+
+
     private void coffeeRoom(){
         while(energy > 0 && energy < 100)
         if (lock.tryLock()) {
@@ -44,7 +94,8 @@ public class Person implements Runnable{
     }
 
     private void exitRoom(){
-        quit = null;
+        this.quit = null;
+        System.out.println(name + "GOING HOME");
     }
 
     private synchronized void useCoffeeMachine(){
